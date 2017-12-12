dd2ba5fe0e2d0279b29675620df3be8afb7e75d2
Michael Ida <31176432+drmikeida@users.noreply.github.com>
2017-12-12 07:49:22 -1000

    Update Queue.java
    readied for deployment

--- Queue.java
+++ Queue.java
@@ -1,42 +1,9 @@
-
+/*
+ * This is the class that implements the queue data structure.
+ * You may decide what data structure to use to implement it.
+ */
 public class Queue {
 
-	private Node head, tail;
-
-	public Queue() {
-		head = null;
-		tail = null;
-	}
-	
-	public void enqueue(String record) {
-		Node newNode = new Node(record);
-		if (head == null) {
-			head = newNode;
-			tail = newNode;
-		} else {
-			tail.next = newNode;
-			newNode.prev = tail;
-			tail = newNode;
-		}
-	}
-	
-	public String dequeue() {
-		String returnRecord = "";
-		if (head != null) {
-			returnRecord = head.record;
-			head = head.next;
-			head.prev = null;
-		}
-		return returnRecord;
-	}
-	
-	public void printQueue() {
-		Node pointer = head;
-		while (pointer != null) {
-			System.out.println(pointer.record);
-			pointer = pointer.next;
-		}
-		System.out.println();
-	}
+	// Your implementation here
 	
 }
